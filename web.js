2b5c4de96bc8a89a27883b366fe69a74f7da06ed
Peter Seibel <peter@gigamonkeys.com>
2022-06-17 23:03:54 -0400

    Better filling in of table.

--- web.js
+++ web.js
@@ -1,15 +1,10 @@
 import { Calendar } from "./calendar.js";
 
-const loadCalendar = (filename) => {
-  fetch(filename)
-    .then(jsonOrBarf)
-    .then((data) => fillCalendar(new Calendar(data)));
-};
-
-const loadSyllabus = (filename) => {
-  fetch(filename)
-    .then(jsonOrBarf)
-    .then((data) => fillSyllabus(data));
+const loadData = (calendar, syllabus) => {
+  Promise.all([
+    fetch(calendar).then(jsonOrBarf),
+    fetch(syllabus).then(jsonOrBarf),
+  ]).then((values) => fillTable(...values));
 };
 
 const jsonOrBarf = (r) => {
@@ -19,52 +14,64 @@ const jsonOrBarf = (r) => {
   return r.json();
 };
 
-const fillCalendar = (cal) => {
+const fillTable = (calendar, syllabus) => {
+  console.log(calendar);
+  console.log(syllabus);
+
   const tbody = document.getElementById("body");
-  cal.elements.forEach((e) => {
+
+  new Calendar(calendar).elements.forEach((e) => {
     if ("weekstring" in e) {
-      tbody.appendChild(weekRow(e));
+      tbody.appendChild(weekRow(e, syllabus));
     } else {
       tbody.appendChild(vacationRow(e));
     }
   });
-  loadSyllabus("syllabus.json");
 };
 
-const fillSyllabus = (data) => {
-  const days = document.querySelectorAll("td.instruction");
-
-  let day = 0;
-  data.forEach((item) => {
-    console.log(item);
-    let d = "start" in item ? item.start : day;
-    console.log(`adding at day ${d}`);
-    days[d].innerText = item.title;
-    days[d].setAttribute("colspan", item.days);
-    for (let i = 0; i < item.days - 1; i++) {
-      console.log(`Removing ${d + i + 1}`);
-      days[d + i + 1].remove();
-    }
-    day = d + item.days;
-  });
-};
-
-const weekRow = (w) => {
+const weekRow = (w, syllabus) => {
   const tr = document.createElement("tr");
-  tr.appendChild(td(w.datesOfWeek(), {}));
-  if (w.start.dayOfWeek == 2) {
-    tr.appendChild(td("", { class: "off" }));
-  }
-  w.days.forEach((_) => tr.appendChild(td("", { class: "instruction" })));
-  if (w.end.dayOfWeek == 4) {
-    tr.appendChild(td("", { class: "off" }));
-  }
 
+  tr.appendChild(td(w.datesOfWeek(), {}));
+
+  if (w.start.dayOfWeek == 2) dayOff(tr);
+
+  let days = w.days.length;
+
+  while (days > 0 && syllabus.length > 0) {
+    const item = syllabus.shift();
+    const consumed = Math.min(days, item.days);
+
+    if (item.title) {
+      scheduled(tr, item.title, consumed);
+    } else {
+      unscheduled(tr, consumed);
+    }
+    if (consumed < item.days) {
+      syllabus.unshift({ title: item.title, days: item.days - consumed });
+    }
+    days -= consumed;
+  }
+  if (days > 0) unscheduled(tr, days);
+  if (w.end.dayOfWeek == 4) dayOff(tr);
   return tr;
+};
+
+const dayOff = (tr) => {
+  tr.appendChild(td("", { class: "off" }));
+};
+
+const scheduled = (tr, title, days) => {
+  tr.appendChild(td(title, { class: "scheduled", colspan: days }));
+};
+
+const unscheduled = (tr, days) => {
+  tr.appendChild(td("Unscheduled", { class: "unscheduled", colspan: days }));
 };
 
 const vacationRow = (v) => {
   const tr = document.createElement("tr");
+  tr.setAttribute("class", "vacation");
   tr.appendChild(td(v.vacationString(), { colspan: "6" }));
   return tr;
 };
@@ -78,4 +85,4 @@ const td = (text, attributes) => {
   return e;
 };
 
-loadCalendar("calendar.json");
+loadData("calendar.json", "syllabus.json");
