22f2270751b7a5bf02148386fa84462090e397e6
Peter Seibel <peter@gigamonkeys.com>
2022-07-01 10:53:57 -0700

    Updated publish script.

--- publish.sh
+++ publish.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
 set -eou pipefail
-set -x
 
 dir=$(basename "$(pwd)")
 sha=$(git log --pretty=tformat:%H -1);
 webdir=~/web/www.gigamonkeys.com/misc/$dir/
 
+echo -n "Clearing $webdir ... "
 if [ -d "$webdir" ]; then
     rm -rf "$webdir";
 fi
 mkdir -p "$webdir"
+echo "ok."
+
+echo -n "Copying files ... "
 cp -R "$@" $webdir
+echo "ok."
+
+echo -n "Adding to git ... "
 cd $webdir
 git add -A .
+echo "ok."
+
+echo "Committing ... "
 git commit -m "Publish $dir $sha" .
+echo "ok."
+
+echo "Pushing ... "
 git push
+echo "ok."
