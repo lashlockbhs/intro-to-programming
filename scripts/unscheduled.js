35bceba79ee6db798ba63d2a3d0402f96100aa03
Peter Seibel <peter@gigamonkeys.com>
2022-07-20 16:19:47 -0700

    Seems to have unscheduled.js working again.

--- scripts/unscheduled.js
+++ scripts/unscheduled.js
@@ -1,7 +1,7 @@
 import fs from "fs";
-import { unscheduled, outline } from "./outline.js";
+import { unscheduled, outline } from "../js/modules/outline.js";
 
-fs.readFile("outline.txt", "utf8", (err, data) => {
+fs.readFile("src/calendar/outline.txt", "utf8", (err, data) => {
   if (err) throw err;
   console.log(JSON.stringify(unscheduled(outline(data)), null, 2));
 });
