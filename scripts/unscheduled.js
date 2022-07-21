d0c396282480b81e3c9d9c45057c00fc3ca9c384
Peter Seibel <peter@gigamonkeys.com>
2022-07-20 21:49:16 -0700

    Fixing path to outline.txt

--- scripts/unscheduled.js
+++ scripts/unscheduled.js
@@ -1,7 +1,7 @@
-import fs from "fs";
-import { unscheduled, outline } from "../js/modules/outline.js";
+import fs from 'fs';
+import { unscheduled, outline } from '../js/modules/outline.js';
 
-fs.readFile("src/calendar/outline.txt", "utf8", (err, data) => {
+fs.readFile('calendar/outline.txt', 'utf8', (err, data) => {
   if (err) throw err;
   console.log(JSON.stringify(unscheduled(outline(data)), null, 2));
 });
