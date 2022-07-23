0d3ee9884b2b9d48dd44055d001f4df136720ccc
Peter Seibel <peter@gigamonkeys.com>
2022-07-23 12:04:15 -0700

    Expressions with results table. Still needs some polish.

--- js/expressions-game.js
+++ js/expressions-game.js
@@ -1,75 +1,165 @@
 import { $, $$ } from './modules/whjqah';
+import makeTable from './modules/table';
 
-const circle = '⚬'
+const circle = '⚬';
 const checkmark = '✓';
 const x = '×';
 
-const allExpression = $$('.expressions .expression');
-
-allExpression.forEach((e) => {
-  e.hidden = true;
-  document.querySelector('.expressions .marks').appendChild(document.createTextNode(circle));
-});
-
 const generators = {
   positive: () => 1 + Math.floor(Math.random() * 100),
-  number: () => -100 + Math.random() * 200,
-}
+  number: () => -100 + Math.random() * 2 * 100,
+};
 
-const checker = (expression, answer) => {
-  const input = expression.dataset.variables.split(',').map((s) => s.split(':'));
-  const vars = input.map((x) => x[0]);
-  const gens = input.map((x) => generators[x[1]]);
-  const expectedFn = Function(vars, `return (${expression.dataset.expression});`);
-  const gotFn = Function(vars, `return (${answer});`);
+class Expressions {
+  constructor(divs, marks) {
+    this.expressions = Array.from(divs).map((div, i) => new Expression(this, div, i));
+    this.answers = {};
+    this.i = 0;
+    this.current = this.expressions[this.i];
+    this.expressions.forEach((e) => {
+      e.hide();
+      marks.appendChild(e.marker);
+    });
+  }
 
-  return () => {
-    const args = gens.map((g) => g());
-    const expected = expectedFn(...args);
-    try {
-      const got = gotFn(...args);
-      const passed = expected === got;
-      return {args, expected, got, passed};
-    } catch (e) {
-      return {args, expected, e, passed: false };
+  switchTo(exp) {
+    this.current.hide();
+    this.current = exp;
+    this.current.show();
+  }
+
+  next() {
+    const currentIndex = this.current.index;
+    for (let i = 0; i < this.expressions.length; i++) {
+      const j = (i + currentIndex + 1) % this.expressions.length;
+      const exp = this.expressions[j];
+      if (!exp.correct) {
+        return exp;
+      }
     }
+    return void 0;
   }
 }
 
-let i = 0;
-let current = allExpression[i];
-current.hidden = false;
+class Expression {
+  constructor(expressions, div, index) {
+    this.expressions = expressions;
+    this.div = div;
+    this.index = index;
 
-const nextExpression = () => {
-  current.hidden = true;
+    this.name = div.querySelector('h1').innerText;
+    this.marker = $('<span>', circle);
+    this.answer = null;
 
-  i += 1;
-  if (i == allExpression.length) {
-    $('#expression-input').hidden = true;
-    $('#results').replaceChildren(document.createTextNode('Good job!'));
+    const input = div.dataset.variables.split(',').map((s) => s.split(':'));
+    this.variables = input.map((x) => x[0]);
+    this.generators = input.map((x) => generators[x[1]]);
+    this.expectedFn = Function(this.variables, `return (${div.dataset.expression});`);
+
+    this.marker.onclick = () => expressions.switchTo(this);
+  }
+
+  hide() {
+    this.div.hidden = true;
+  }
+
+  show() {
+    this.div.hidden = false;
+  }
+
+  check(answer) {
+    this.answer = answer;
+    this.results = Array(100).fill().map(this.checker(answer));
+    this.correct = this.results.every((r) => r.passed);
+    this.marker.replaceChildren(document.createTextNode(this.correct ? checkmark : x));
+    return this.correct;
+  }
+
+  checker(answer) {
+    const fn = Function(this.variables, `return (${answer});`);
+
+    return () => {
+      const args = this.generators.map((g) => g());
+      const expected = this.expectedFn(...args);
+      try {
+        const got = fn(...args);
+        const passed = equal(expected, got);
+        return { args, expected, got, passed };
+      } catch (e) {
+        return { args, expected, e, passed: false };
+      }
+    };
+  }
+}
+
+const equal = (a, b) => {
+  if (Number.isInteger(a) && Number.isInteger(b)) {
+    return a === b;
   } else {
-    current = allExpression[i];
-    current.hidden = false;
-    $('#expression-input').value = '';
+    // Check non integers by relative tolerance.
+    const relError = Math.abs(a - b) / Math.abs((a + b) / 2);
+    // This is probably way too conservative
+    return relError <= 0.00001;
   }
 };
 
+const completedTable = () => {
+  const t = makeTable();
+  t.addColumn('Problem', 'problem');
+  t.addColumn('Answer', 'answer');
+  t.addColumn('Ok?', 'ok');
+  return t;
+};
+
+const problems = (expr) => {
+  const t = makeTable();
+  t.table.className = 'problems';
+  expr.variables.forEach((v) => t.addColumn(v, 'variable'));
+  t.addColumn('Expected', 'expected');
+  t.addColumn('Got', 'got');
+  expr.results.forEach((r) => {
+    if (!r.passed) {
+      t.addRow([...r.args, r.expected, r.got ?? r.e]);
+    }
+  });
+  return t.table;
+};
+
+const completed = completedTable();
+
+$('#completed').replaceChildren(completed.table);
+
+const expressions = new Expressions(
+  $$('.expressions .expression'),
+  document.querySelector('.expressions .marks'),
+);
+
+expressions.current.show();
 
 $('#expression-input').onchange = (e) => {
+  const expr = expressions.current;
 
   try {
-    const c = checker(current, e.target.value);
-    const r = Array(100).fill().map(c);
+    expr.check(e.target.value);
 
-    if (r.some((x) => !x.passed)) {
-      $('#results').replaceChildren(document.createTextNode("Uh, oh!"))
-      document.querySelector('.expressions .marks').childNodes[i].replaceWith(document.createTextNode(x));
+    if (!expr.correct) {
+      $('#results').replaceChildren($('<p>', 'Uh, oh!'), problems(expr));
+      completed.addRow([expr.name, expr.answer, '❌']);
     } else {
-      document.querySelector('.expressions .marks').childNodes[i].replaceWith(document.createTextNode(checkmark));
-      nextExpression();
+      $('#expression-input').value = '';
+      completed.addRow([expr.name, expr.answer, '✅']);
+      const n = expressions.next();
+      if (n) {
+        expressions.switchTo(n);
+        $('#results').replaceChildren();
+      } else {
+        expr.hide();
+        $('#expression-input').hidden = true;
+        $('#results').replaceChildren(document.createTextNode('Good job!'));
+      }
     }
   } catch (e) {
     console.log(e);
-    $('#results').replaceChildren(document.createTextNode(`Uh, oh! ${e.name}`))
+    $('#results').replaceChildren(document.createTextNode(`Uh, oh! ${e.name}`));
   }
-}
+};
---
+++ js/modules/table.js
@@ -0,0 +1,34 @@
+import { $, withClass } from './whjqah';
+
+class Table {
+  constructor() {
+    this.table = $('<table>');
+    this.columnClasses = [];
+    this.headerRow = $('<tr>');
+    this.tbody = $('<tbody>');
+
+    const thead = $('<thead>');
+    thead.append(this.headerRow);
+    this.table.append(thead);
+    this.table.append(this.tbody);
+  }
+
+  addColumn(name, clazz) {
+    this.columnClasses.push(clazz);
+    this.headerRow.append(withClass(clazz, $('<th>', name)));
+  }
+
+  addRow(values) {
+    const row = this.tbody.insertRow();
+    values.forEach((value, i) => {
+      const cell = row.insertCell();
+      cell.className = this.columnClasses[i];
+      cell.append(value);
+    });
+    return row;
+  }
+}
+
+const makeTable = (columns) => new Table(columns);
+
+export default makeTable;
