5ee5d49570a963ebd70ae9331a803d60c46e7159
Peter Seibel <peter@gigamonkeys.com>
2022-08-18 07:31:02 -0700

    Simple draw assignment.

---
+++ assignments/simple-draw/frame/framework.js
@@ -0,0 +1,41 @@
+const canvas = document.getElementById('screen');
+const ctx = canvas.getContext('2d');
+const width = canvas.width;
+const height = canvas.height;
+
+const drawLine = (x1, y1, x2, y2, color, width=1) => {
+  ctx.strokeStyle = color;
+  ctx.lineWidth = width;
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+}
+
+const drawCircle = (x, y, r, color, lineWidth=1) => {
+  ctx.strokeStyle = color;
+  ctx.lineWidth = lineWidth;
+  ctx.beginPath();
+  ctx.ellipse(x, y, r, r, 0, 0, 2 * Math.PI);
+  ctx.stroke();
+};
+
+const drawRect = (x, y, width, height, color, lineWidth=1) => {
+  ctx.strokeStyle = color;
+  ctx.lineWidth = lineWidth;
+  ctx.strokeRect(x, y, width, height);
+};
+
+const drawFilledRect = (x, y, width, height, color) => {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+};
+
+const drawFilledCircle = (x, y, r, color) => {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.ellipse(x, y, r, r, 0, 0, 2 * Math.PI);
+  ctx.fill();
+};
+
+const clear = () => ctx.clearRect(0, 0, width, height);
