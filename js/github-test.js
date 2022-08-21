41b7a0097a7dc62da0b1383ada98505e5d9968e4
Peter Seibel <peter@gigamonkeys.com>
2022-08-21 11:07:12 -0700

    Exercise new github stuff.

--- js/github-test.js
+++ js/github-test.js
@@ -9,6 +9,28 @@ const test = async () => {
 
   const gh = await github.connect(siteId);
 
+  out += '\n// Getting repos\n';
+  const repos = await gh.orgRepos('bhs-intro-to-programming').listRepos();
+  //out += toJSON(repos);
+
+  for (const r of repos) {
+    if (!r.raw.is_template) {
+      out += `In ${r.name} code.js exists: `;
+      const exists = await r
+        .newFileExists('assignments/simple-draw/code.js', 'assignments/simple-draw')
+        .catch(() => false);
+      out += exists;
+      out += '\n';
+      /*
+      const text = await r.getFile("assignments/simple-draw/code.js", "assignments/simple-draw")
+            .then((f) => atob(f.content))
+            .catch(() => "Nothing");
+            out += toJSON(text);
+            */
+    }
+  }
+
+  /*
   out += toJSON(gh.user);
   out += '\n// lookiing for gigamonkeys membership\n';
   out += toJSON(await gh.membership('gigamonkeys'));
@@ -40,7 +62,7 @@ const test = async () => {
       required_conversation_resolution: false,
     }),
   );
-
+  */
   // out += `\n// Updating branch pull request review protection\n`;
   // out += toJSON(await r.updateBranchPullRequestReviewProtection('main'));
 
