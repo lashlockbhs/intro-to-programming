ec62e5be743b2f50c75e5160b76abb0a8e1aedf7
Peter Seibel <peter@gigamonkeys.com>
2022-08-21 21:52:29 -0700

    Prettying simple-draw starter code.

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -35,9 +35,9 @@
  *
  */
 
-drawLine(0, 0, width, height, 'red')
+drawLine(0, 0, width, height, 'red');
 
-drawCircle(width/2, height/2, 10, 'blue');
+drawCircle(width / 2, height / 2, 10, 'blue');
 
 drawRect(50, 50, 60, 40, 'black');
 
