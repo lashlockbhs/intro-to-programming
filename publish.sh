c38f775682beb277f44b17ff747b81660005ac0d
Peter Seibel <peter@gigamonkeys.com>
2022-08-24 10:59:14 -0700

    Switch back to old web dir and update actual slides HTML.

--- publish.sh
+++ publish.sh
@@ -11,7 +11,7 @@ fi
 # Ensure dir ends in trailing slash for rsync.
 builddir="${1%/}/"
 
-webdir=/tmp/intro.gigamonkeys.com/
+webdir=~/web/intro.gigamonkeys.com/
 mkdir -p "$webdir"
 
 sha=$(git log --pretty=tformat:%H -1);
